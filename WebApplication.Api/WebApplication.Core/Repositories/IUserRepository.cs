f7110baef9dde48406f7f3a94987715eab7fcb12
Rafał Nowociński <vito24222@gmail.com>
2019-04-04 17:48:14 +0200

    add repository

---
+++ WebApplication.Api/WebApplication.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebApplication.Core.Domain;
+
+namespace WebApplication.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetAsync(Guid Id);
+        Task<User> GetAsync(string Email);
+        Task AddAsync(User User);
+        Task UpdateAsync(User User);
+        Task DeleteAsync(User User);
+    }
+}
---
+++ WebApplication.Api/WebApplication.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using WebApplication.Core.Domain;
+using WebApplication.Core.Domain.Context;
+using WebApplication.Core.Repositories;
+
+namespace WebApplication.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataBaseContext _context;
+
+        public UserRepository(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetAsync(Guid Id)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.Id == Id);
+            return await Task.FromResult(user);
+        }
+
+        public async Task<User> GetAsync(string Email)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.Email == Email);
+            return await Task.FromResult(user);
+        }
+
+        public async Task AddAsync(User User)
+        {
+            _context.Users.Add(User);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateAsync(User User)
+        {
+            _context.Users.Update(User);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(User User)
+        {
+            _context.Accounts.Remove(User);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+    }
+}
