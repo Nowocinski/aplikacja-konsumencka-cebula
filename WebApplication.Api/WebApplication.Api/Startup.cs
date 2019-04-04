5d83064f39d7efa9dbff2441206f4bdd286e3bda
Rafał Nowociński <vito24222@gmail.com>
2019-04-04 17:14:09 +0200

    add parameters to connect with database

--- WebApplication.Api/WebApplication.Api/Startup.cs
+++ WebApplication.Api/WebApplication.Api/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApplication.Core.Domain.Context;
 
 namespace WebApplication.Api
 {
@@ -26,6 +28,11 @@ namespace WebApplication.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Łączenie się z bazą danych MS SQL
+            services.AddDbContext<DataBaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DevConnection"), b => b.MigrationsAssembly("WebApplication.Api"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
