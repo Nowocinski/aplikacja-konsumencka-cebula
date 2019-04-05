e0e8d1915a3d17db1ab08879ba03b77374a8a722
Rafał Nowociński <vito24222@gmail.com>
2019-04-05 09:24:33 +0200

    add class DTO to return user date

--- WebApplication.Api/WebApplication.Infrastructure/DTO/AccountDTO.cs
+++ WebApplication.Api/WebApplication.Infrastructure/DTO/AccountDTO.cs
@@ -1,14 +1,10 @@
-﻿using System;
-
-namespace WebApplication.Infrastructure.DTO
+﻿namespace WebApplication.Infrastructure.DTO
 {
     public class AccountDTO
     {
-        public string Token { get; set; }
-        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
