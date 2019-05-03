58fc0bc98a192a6e581a0f066ed90f2391a79f1e
Rafał Nowociński <vito24222@gmail.com>
2019-05-03 18:52:46 +0200

    column type setting in the database

--- WebApplication.Api/WebApplication.Core/Domain/Context/DataBaseContext.cs
+++ WebApplication.Api/WebApplication.Core/Domain/Context/DataBaseContext.cs
@@ -4,96 +4,113 @@ namespace WebApplication.Core.Domain.Context
 {
     public class DataBaseContext : DbContext
     {
-        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>(column =>
             {
                 column.Property(name => name.FirstName)
-                      .IsRequired(true)
-                      .HasMaxLength(20);
+                      .HasColumnType("nvarchar(20)")
+                      .IsRequired(true);
                 column.Property(name => name.LastName)
-                      .IsRequired(true)
-                      .HasMaxLength(30);
+                      .HasColumnType("nvarchar(30)")
+                      .IsRequired(true);
                 column.Property(name => name.PhoneNumber)
-                      .IsRequired(true)
-                      .HasMaxLength(11);
+                      .HasColumnType("nvarchar(11)")
+                      .IsRequired(true);
                 column.Property(name => name.Email)
-                      .IsRequired(true)
-                      .HasMaxLength(40);
+                      .HasColumnType("nvarchar(40)")
+                      .IsRequired(true);
                 column.Property(name => name.Password)
-                      .IsRequired(true)
-                      .HasMaxLength(100);
+                      .HasColumnType("nvarchar(100)")
+                      .IsRequired(true);
+
             });
 
             modelBuilder.Entity<Message>(column =>
             {
                 column.Property(name => name.Sender_Id)
+                      .HasColumnType("uniqueidentifier")
                       .IsRequired(true);
                 column.Property(name => name.Recipient_Id)
+                      .HasColumnType("uniqueidentifier")
                       .IsRequired(true);
                 column.Property(name => name.Contents)
+                      .HasColumnType("nvarchar(MAX)")
                       .IsRequired(true);
                 column.Property(name => name.Date)
+                      .HasColumnType("datetime")
                       .IsRequired(true);
             });
 
             modelBuilder.Entity<Advertisement>(column =>
             {
                 column.Property(name => name.User_Id)
+                      .HasColumnType("uniqueidentifier")
                       .IsRequired(true);
                 column.Property(name => name.Title)
-                      .IsRequired(true)
-                      .HasMaxLength(25);
-                column.Property(name => name.Description)
-                      .IsRequired(true)
-                      .HasMaxLength(500);
-                column.Property(name => name.Price)
+                      .HasColumnType("nvarchar(25)")
                       .IsRequired(true);
-                column.Property(name => name.City_Id)
+                column.Property(name => name.Description)
+                      .HasColumnType("nvarchar(500)")
+                      .IsRequired(true);
+                column.Property(name => name.Price)
+                      .HasColumnType("decimal(20, 2)")
+                      .IsRequired(true);
+                column.Property(name => name.City_id)
+                      .HasColumnType("int")
                       .IsRequired(true);
                 column.Property(name => name.Street)
-                      .IsRequired(true)
-                      .HasMaxLength(50);
+                      .HasColumnType("nvarchar(50)")
+                      .IsRequired(true);
                 column.Property(name => name.Floor)
+                      .HasColumnType("int")
                       .IsRequired(false);
                 column.Property(name => name.Size)
+                      .HasColumnType("decimal(20,2)")
                       .IsRequired(true);
                 column.Property(name => name.Category)
-                      .IsRequired(true)
-                      .HasMaxLength(30);
+                      .HasColumnType("nvarchar(30)")
+                      .IsRequired(true);
                 column.Property(name => name.Date)
+                      .HasColumnType("datetime")
                       .IsRequired(true);
             });
 
             modelBuilder.Entity<AdvertisementImage>(column =>
             {
                 column.Property(name => name.Advertisement_Id)
+                      .HasColumnType("uniqueidentifier")
                       .IsRequired(true);
                 column.Property(name => name.Image)
+                      .HasColumnType("nvarchar(MAX)")
                       .IsRequired(true);
                 column.Property(name => name.Description)
-                      .IsRequired(true)
-                      .HasMaxLength(100);
+                      .HasColumnType("nvarchar(100)")
+                      .IsRequired(true);
                 column.Property(name => name.Name)
-                      .IsRequired(true)
-                      .HasMaxLength(100);
+                      .HasColumnType("nvarchar(100)")
+                      .IsRequired(true);
             });
 
             modelBuilder.Entity<Voivodeship>(column =>
             {
                 column.Property(name => name.Name)
-                      .IsRequired(true)
-                      .HasMaxLength(30);
+                      .HasColumnType("nvarchar(30)")
+                      .IsRequired(true);
             });
 
             modelBuilder.Entity<City>(column =>
             {
                 column.Property(name => name.Name)
-                      .IsRequired(true)
-                      .HasMaxLength(40);
+                      .HasColumnType("nvarchar(40)")
+                      .IsRequired(true);
                 column.Property(name => name.Voivodeship_Id)
+                      .HasColumnType("int")
                       .IsRequired(true);
             });
 
@@ -112,7 +129,7 @@ namespace WebApplication.Core.Domain.Context
             modelBuilder.Entity<Advertisement>()
                         .HasOne(a => a.City)
                         .WithMany(b => b.Advertisement)
-                        .HasForeignKey(x => x.City_Id)
+                        .HasForeignKey(x => x.City_id)
                         .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Message>()
