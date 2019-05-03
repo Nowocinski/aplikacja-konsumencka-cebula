d2736950005228517006da3845262b97692df5ae
Rafał Nowociński <vito24222@gmail.com>
2019-05-03 10:46:28 +0200

    improve method

--- WebApplication.Api/WebApplication.Infrastructure/Extensions/RemoveDiacriticsExtensions.cs
+++ WebApplication.Api/WebApplication.Infrastructure/Extensions/RemoveDiacriticsExtensions.cs
@@ -1,40 +1,41 @@
-﻿namespace WebApplication.Infrastructure.Extensions
+﻿using System.Text;
+
+namespace WebApplication.Infrastructure.Extensions
 {
     public static class RemoveDiacriticsExtensions
     {
         public static string RemoveDiacritics(this string text)
         {
-            string newText = "";
-
-            foreach(var t in text)
+            StringBuilder newText = new StringBuilder(text.Length);
+            foreach (var t in text)
             {
                 switch(t)
                 {
-                    case 'ą': newText += "a"; break;
-                    case 'ć': newText += "c"; break;
-                    case 'ę': newText += "e"; break;
-                    case 'ł': newText += "l"; break;
-                    case 'ń': newText += "n"; break;
-                    case 'ó': newText += "o"; break;
-                    case 'ś': newText += "s"; break;
-                    case 'ź': newText += "z"; break;
-                    case 'ż': newText += "z"; break;
+                    case 'ą': newText.Append("a"); break;
+                    case 'ć': newText.Append("c"); break;
+                    case 'ę': newText.Append("e"); break;
+                    case 'ł': newText.Append("l"); break;
+                    case 'ń': newText.Append("n"); break;
+                    case 'ó': newText.Append("o"); break;
+                    case 'ś': newText.Append("s"); break;
+                    case 'ź': newText.Append("z"); break;
+                    case 'ż': newText.Append("z"); break;
 
-                    case 'Ą': newText += "A"; break;
-                    case 'Ć': newText += "C"; break;
-                    case 'Ę': newText += "E"; break;
-                    case 'Ł': newText += "L"; break;
-                    case 'Ń': newText += "N"; break;
-                    case 'Ó': newText += "O"; break;
-                    case 'Ś': newText += "S"; break;
-                    case 'Ź': newText += "Z"; break;
-                    case 'Ż': newText += "Z"; break;
+                    case 'Ą': newText.Append("A"); break;
+                    case 'Ć': newText.Append("C"); break;
+                    case 'Ę': newText.Append("E"); break;
+                    case 'Ł': newText.Append("L"); break;
+                    case 'Ń': newText.Append("N"); break;
+                    case 'Ó': newText.Append("O"); break;
+                    case 'Ś': newText.Append("S"); break;
+                    case 'Ź': newText.Append("Z"); break;
+                    case 'Ż': newText.Append("Z"); break;
 
-                    default: newText += t; break;
+                    default: newText.Append(t); break;
                 }
             }
 
-            return newText;
+            return newText.ToString();
         }
     }
 }
